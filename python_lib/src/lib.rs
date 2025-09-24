a0b8f3567ea85aa7c53a46582890c476b05ff2a1
Charles-Emmanuel Teuf <charles-emmanuel.teuf@protonmail.com>
2025-09-24 12:25:01 -0400

    initial commit

--- python_lib/src/lib.rs
+++ python_lib/src/lib.rs
@@ -10,5 +10,6 @@ fn py_calculer(a: f64, b: f64) -> PyResult<f64> {
 fn ftp_core_python(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(py_calculer, m)?)?;
     println!("Hello, world!");
+    println!("Hello, world!2");
     Ok(())
 }
